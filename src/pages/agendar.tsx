0c0a80094da028a0180aeac3088a6c069d9b32f7
sistema-delivery <vitoriareels081@gmail.com>
2025-04-23 19:24:21 -0300

    Update agendar.tsx

--- src/pages/agendar.tsx
+++ src/pages/agendar.tsx
@@ -1,12 +1,12 @@
 // src/pages/agendar.tsx
-import Layout from '@/components/Layout'
-import AppointmentForm from '@/components/AppointmentForm'  // 1) importe seu form
+import Layout from '../components/Layout'
+import AppointmentForm from '../components/AppointmentForm'
 
 export default function Agendar() {
   return (
     <Layout>
       <h1 className="text-2xl font-bold mb-6">Agendar Corte</h1>
-      <AppointmentForm />                              {/* 2) renderize o formulário */}
+      <AppointmentForm />
     </Layout>
   )
 }
