bc645b705860d7e7adaf1b406003c076c4c9e470
sistema-delivery <vitoriareels081@gmail.com>
2025-04-23 16:00:35 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,8 @@
-
+// pages/index.tsx
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Bem-vindo ao SistemaBarbers!</h1>
+    </div>
+  )
+}
