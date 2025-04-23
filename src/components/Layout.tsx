808aa6aac96aa593474fe0a7cae0cba49c48fdf1
sistema-delivery <vitoriareels081@gmail.com>
2025-04-23 19:26:41 -0300

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,1 +1,19 @@
+import React, { ReactNode } from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
+interface LayoutProps {
+  children: ReactNode
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-50">
+      <Navbar />
+      <main className="flex-grow container mx-auto px-4 py-8">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  )
+}
