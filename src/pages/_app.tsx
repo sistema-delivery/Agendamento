cee3db805c0447b9590390167597ead7d7a75932
sistema-delivery <vitoriareels081@gmail.com>
2025-04-24 22:10:16 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
 // src/pages/_app.tsx
-import '../styles/globals.css'
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { useState } from 'react'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps<{ initialSession: Session }>) {
+  // Cria o client no browser para lidar com hash e sessão
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  )
 }
-
-export default MyApp
